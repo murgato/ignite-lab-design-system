b0900c27918b9642ab91aacaa17161bed234b9c7
Murilo Couto <murilo.couto.santos@hotmail.com>
2022-10-13 11:17:46 -0300

    create form of login

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,19 +1,26 @@
-import { ReactNode } from "react";
+import { ReactNode, ButtonHTMLAttributes } from "react";
 import { clsx } from "clsx";
 import { Slot } from "@radix-ui/react-slot";
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({
+  children,
+  asChild,
+  className,
+  ...props
+}: ButtonProps) {
   const Compo = asChild ? Slot : "button";
   return (
     <Compo
       className={clsx(
-        "py-4 px-3 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring=2 ring-white"
+        "py-3 px-4 bg-cyan-500 rounded font-semibold text-black text-sm w-full transition-colors hover:bg-cyan-300 focus:ring=2 ring-white",
+        className
       )}
+      {...props}
     >
       {children}
     </Compo>
